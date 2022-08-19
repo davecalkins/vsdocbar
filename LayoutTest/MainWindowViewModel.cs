136a7d79c4088f6993253404b9626a6bb57bc016
davecalkins <2372599+davecalkins@users.noreply.github.com>
2022-08-18 22:37:10 -0400

    added layout test project to illustrate issue with grid layout

---
+++ LayoutTest/MainWindowViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LayoutTest
+{
+    internal class ObservablePerson : ViewModelBase
+    {
+        private string _name;
+        public string Name
+        {
+            get { return "Person's Name: " + _name; }
+            set { SetProperty(ref _name, value); }
+        }
+    }
+
+    internal class MainWindowViewModel : ViewModelBase
+    {
+        private ObservableCollection<ObservablePerson> _people = new ObservableCollection<ObservablePerson>();
+        public ObservableCollection<ObservablePerson> People
+        {
+            get { return _people;  }
+            set { SetProperty(ref _people, value); }
+        }
+
+        public void Initialize()
+        {
+            var testPeople = new ObservablePerson[]
+            {
+                new ObservablePerson { Name = "Donald Duck" },
+                new ObservablePerson { Name = "James T. Kirk" },
+                new ObservablePerson { Name = "Obi-Wan Kenobi" },
+                new ObservablePerson { Name = "Gandalf" },
+                new ObservablePerson { Name = "Michael Knight" },
+                new ObservablePerson { Name = "Stringfellow Hawke" },
+                new ObservablePerson { Name = "Dominic Santini" }
+            };
+
+            People = new ObservableCollection<ObservablePerson>(testPeople);
+        }
+    }
+}
---
+++ LayoutTest/ViewModelBase.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace LayoutTest
+{
+    /// <summary>
+    /// base view model class which provides basic property changed notifications supporting binding from XAML
+    /// </summary>
+    internal class ViewModelBase : INotifyPropertyChanged
+    {
+        protected bool SetProperty<T>(ref T backingStore, T value,
+            [CallerMemberName]string propertyName = "",
+            Action onChanged = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(backingStore, value))
+                return false;
+
+            backingStore = value;
+            onChanged?.Invoke();
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            var changed = PropertyChanged;
+            if (changed == null)
+                return;
+
+            changed.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+
+        public virtual Task InitializeAsync(object data)
+        {
+            return Task.FromResult(false);
+        }
+    }
+}
